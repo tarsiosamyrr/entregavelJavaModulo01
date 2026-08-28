d9b0a8639c712b45a9ede285ae050e2d46b48495
Társio Samyr <tarsiosamyrr@gmail.com>
2026-08-28 12:39:15 -0300

    Entreável de POO

---
+++ EntregavelJava/src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+
+    String nome;
+    int idade;
+    String profissao;
+
+    public Pessoa(String nome, int idade, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+}
